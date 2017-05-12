f0e8c0e38b3d693e6024098cbe9e5b80603bba5a
Siming Chen <siming.chen@sap.com>
2017-05-12 15:58:02 +0800

    Add:config.sample.js

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # kindle
 生产mobi文件并推送到指定邮箱
+
+
+##配置
+
+复制config.sample.js并生产和配置config.js
+
+##Run and Build
---
+++ config.sample.js
@@ -0,0 +1,35 @@
+const config;
+const message;
+const nodemailer;
+const bookDir;
+
+//邮件模板
+message = {
+	  from: '',
+    to: '',
+    subject: 'mobi title',
+    text: 'Plaintext version of the message',
+    html: '<p>HTML version of the message</p>',
+    attachments :[
+        {   // utf-8 string as an attachment
+            filename: 'book.epub',
+            content: fs.readFileSync('./sourceBooks/1.epub')
+        }]
+    };
+//邮件服务器配置
+nodemailer ={};
+nodemailer.service='QQ';
+nodemailer.auth = {
+	  user: '@qq.com',
+        pass: ''
+}
+
+//文件导入导出目录
+bookDir ={
+	sourceBooks:'./sourceBooks',
+	targetBooks:'./targetBooks'
+}
+config={
+	message:message,
+	nodemailer:nodemailer
+}
